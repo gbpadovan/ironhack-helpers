9e6dc2961a9a752cfacf3d8a23d7d37b9e80df61
andreaguiar <ufabc.andre@gmail.com>
2019-07-23 20:13:17 -0300

    Create installable module by adding setup.py
    Signed-off-by: andreaguiar <ufabc.andre@gmail.com>

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+   name='IronHelpers',
+   version='0.0.1',
+   author='Data Analytics Part Time - Turma 1',
+   author_email='@ironhack.com',
+   packages=['ironhelpers', ],
+   url='',
+   license='LICENSE.txt',
+   description='Basic helper functions created for the Data Analytics class',
+   long_description=open('README.md').read(),
+   install_requires=[],
+)
